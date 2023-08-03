3aa2823e6ec440099aafa49acd21510ff9832e24
Oleg Markov <o.mark-mail.ru@yandex.ru>
2023-08-03 23:15:21 +0200

    доработана страница main

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -14,7 +14,12 @@ function AboutProject() {
               <p className="project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
             </div>
         </div>
-
+        <div className="project__table">
+          <h3 className="project__week">1 неделя</h3>
+          <h3 className="project__weeks">4 недели</h3>
+          <p className="project__speciality">Back-end</p>
+          <p className="project__speciality">Front-end</p>
+        </div>
     </section>
   )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,13 @@ function Footer() {
   return (
     <footer className="footer">
         <p className="footer__about">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-        <p>© 2023</p>
-
+        <div className="footer__box">
+          <p className="footer__year">© 2023</p>
+          <div className="footer__links">
+            <a className="footer__yp" href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
+            <a className="footer__git" href="https://github.com/yandex-praktikum/">Github</a>
+          </div>
+        </div>
     </footer>
   )
 }
